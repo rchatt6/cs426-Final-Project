fe2913255b3a7550f95a687ad0bdd00b4afbc475
abreyer3 <abreye2@uic.edu>
2020-03-20 06:30:15 -0500

    basic zombie anim added with chase script

---
+++ Assets/Scripts/TriggerPlayerDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlayerDamage : MonoBehaviour
+{
+
+    public int health = 100;
+    int damage = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(health);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            health -= damage;
+            Debug.Log(health);
+            if (health <= 0)
+            {
+                Debug.Log("you died" + health);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
