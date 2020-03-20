4ed7fe44c658b110048620919b03fd611e755475
rchatt6 <rahulchat0007@gmail.com>
2020-03-20 17:17:26 -0500

    Floating Objects

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/damagePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagePlayer : MonoBehaviour
+{
+    public int health = 100;
+    int damage = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(health);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "DeathPlane")
+        {
+            Destroy(this.gameObject);
+            health -= damage;
+            Debug.Log("you died" + health);
+
+        }
+    }
+}
