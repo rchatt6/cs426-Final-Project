86c3668ce8b106457fa362e2f376c1de1e00ce69
abreyer3 <abreye2@uic.edu>
2020-03-25 07:43:40 -0500

    stamina fix, added audio, zombie anims & hitbox

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static float CurrentHealth { get; set; }
+    public static float MaxHealth { get; set; }
+    public static float CurrentArmor { get; set; }
+    public static float MaxArmor { get; set; }
+    public static bool hasArmor = true;
+
+    public Slider healthbar;
+    public Slider armorbar;
+
+    public Text healthText;
+    public Text armorText;
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 100f;
+        CurrentHealth = MaxHealth;
+        MaxArmor = 90f;
+        CurrentArmor = MaxArmor;
+
+        healthText.text = CurrentHealth + "/" + MaxHealth;
+        armorText.text = CurrentArmor + "/" + MaxArmor;
+
+        healthbar.value = CalculateHealth();
+        armorbar.value = CalculateArmor();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "DeathPlane") || (other.tag == "Zombie"))
+        {
+            //rb.MovePosition(transform.position + transform.right * Time.fixedDeltaTime);
+            //rb.transform.position = new Vector3(transform.position.x, transform.position.y + 2.0f, transform.position.z);
+
+            if (hasArmor && CurrentHealth == 100)
+            {
+                DealArmorDamage(30);
+            }
+
+            if (!hasArmor || CurrentArmor <= 0)
+            {
+                DealDamage(20);
+            }
+        }
+    }
+
+    void DealArmorDamage(float damageValue)
+    {
+        CurrentArmor -= damageValue;
+        armorbar.value = CalculateArmor();
+        armorText.text = CurrentArmor + "/" + MaxArmor;
+    }
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+        healthbar.value = CalculateHealth();
+        healthText.text = CurrentHealth + "/" + MaxHealth;
+
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    float CalculateArmor()
+    {
+        return CurrentArmor / MaxArmor;
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("You died");
+    }
+}
---
+++ Assets/Scripts/TriggerPlayerDamage.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlayerDamage : MonoBehaviour
+{
+    //public float CurrentHealth { get; set; }
+    //public float MaxHealth { get; set; }
+    //public float CurrentArmor { get; set; }
+    //public float MaxArmor { get; set; }
+    //public bool hasArmor = true;
+
+    //public float health = 100;
+    //public float armor = 90;
+    //public float damage = 20;
+
+    private AudioSource m_AudioSource;
+    [SerializeField] private AudioClip hurtSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerHealth.MaxHealth = 100;
+        PlayerHealth.CurrentHealth = PlayerHealth.MaxHealth;
+
+        PlayerHealth.MaxArmor = 90;
+        PlayerHealth.CurrentArmor = PlayerHealth.MaxArmor;
+
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_AudioSource.clip = hurtSound;
+            m_AudioSource.Play();
+
+            if (PlayerHealth.hasArmor && PlayerHealth.CurrentHealth == 100)
+            {
+                //DealArmorDamage(30);
+
+            }
+
+            if (!PlayerHealth.hasArmor || PlayerHealth.CurrentArmor <= 0)
+            {
+                //DealDamage(20);
+            }
+            
+            if (PlayerHealth.CurrentHealth <= 0)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    void DealArmorDamage(float damageValue)
+    {
+        PlayerHealth.CurrentArmor -= damageValue;
+    }
+
+    void DealDamage(float damageValue)
+    {
+        PlayerHealth.CurrentHealth -= damageValue;
+    }
+}
