320bdbd8590ed2dfef83af129939423d89b8bbf5
R chat <rahulchat0007@gmail.com>
2020-04-21 15:22:30 -0500

    Beta

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+    [SyncVar(hook ="UpdateHealth")]public int currentHealth = maxHealth;
+
+    [SerializeField]
+    private Stats health;
+
+    private GameObject respawnPoint;
+
+    public static int teamNum;
+
+    private void Awake()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        /*if (NetworkServer.connections.Count % 2 == 0)
+        {
+            teamNum = 2;
+        }
+        else
+        {
+            teamNum = 1;
+        }
+
+        Debug.Log("Initialized! You are on team " + teamNum +"!");
+
+        health.Initialize();*/
+    }
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (NetworkServer.connections.Count % 2 == 0)
+        {
+            teamNum = 2;
+        }
+        else
+        {
+            teamNum = 1;
+        }
+
+        Debug.Log("Initialized! You are on team " + teamNum + "!");
+
+        health.Initialize();
+
+        currentHealth = maxHealth;
+        health.MaxVal = maxHealth;
+        health.currentVal = maxHealth;
+
+        //Debug.Log("Start!");
+    }
+
+    public void UpdateHealth(int old, int current)
+    {
+        //old = current;
+        current = old;
+    }
+
+    public void TakeDamage(int amount, Collider collider)
+    {
+
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        health.currentVal -= amount;
+
+        Debug.Log(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            health.currentVal = 0;
+            Debug.Log("DEAD!");
+            if (teamNum == 1)
+            {
+                respawnPoint = GameObject.FindGameObjectWithTag("Respawn1");
+            }
+            else
+            {
+                respawnPoint = GameObject.FindGameObjectWithTag("Respawn2");
+            }
+            
+            collider.transform.position = respawnPoint.transform.position;
+            currentHealth = 100;
+            health.currentVal = 100;
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerPlayerDamage2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class TriggerPlayerDamage2 : NetworkBehaviour
+{
+    private AudioSource m_AudioSource;
+    [SerializeField]
+    private AudioClip hurtSound;
+    [SerializeField]
+    private int damageValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (!isServer)
+            //return;
+
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        //GameObject hit = collider.gameObject;
+        Health health = collider.GetComponent<Health>();
+
+        if (health != null)
+        {
+            m_AudioSource.clip = hurtSound;
+            m_AudioSource.Play();
+            health.TakeDamage(damageValue, collider);
+        }
+    }
+}
